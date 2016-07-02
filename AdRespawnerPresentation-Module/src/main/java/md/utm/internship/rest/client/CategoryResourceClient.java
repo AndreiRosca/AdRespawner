34b208e7caa3c33b28cf5c869b3d35d76c0ecbac
AndreiRosca <speculum@mail.com>
2016-07-02 20:28:42 +0300

    Added the CategoryResourceClient

---
+++ AdRespawnerPresentation-Module/src/main/java/md/utm/internship/rest/client/CategoryResourceClient.java
@@ -0,0 +1,61 @@
+package md.utm.internship.rest.client;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+public class CategoryResourceClient {
+
+	private Client client = ClientBuilder.newClient();
+	private WebTarget target;
+	
+	public CategoryResourceClient(String categoryResourceUrl) {
+		target = client.target(categoryResourceUrl);
+	}
+	
+	public List<Category> getAllCategories() {
+		return target.path("/categories")
+					 .request()
+					 .accept(MediaType.APPLICATION_JSON)
+					 .get(new GenericType<List<Category>>() {});
+	}
+	
+	public Category getCategory(Long id) {
+		return target.path("/categories/")
+					 .path(id.toString())
+					 .request()
+					 .accept(MediaType.APPLICATION_JSON)
+					 .get(new GenericType<Category>() {});
+	}
+	
+	public Category createCategory(Category category) {
+		return target.path("/categories")
+					 .request()
+					 .accept(MediaType.APPLICATION_JSON)
+					 .post(Entity.json(category), Category.class);
+	}
+	
+	public Category updateCategory(Long id, Category category) {
+		return target.path("/categories/")
+					 .path(id.toString())
+					 .request()
+					 .accept(MediaType.APPLICATION_JSON)
+					 .put(Entity.json(category), Category.class);
+	}
+	
+	public void deleteCategory(Long id) {
+		target.path("/categories/")
+			  .path(id.toString())
+			  .request()
+			  .delete();
+	}
+	
+	public void dispose() {
+		client.close();
+	}
+}
